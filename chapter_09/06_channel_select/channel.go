e4e905101ef3c0ce4b0e6e9ffaf60ea4bc17c7e6
Kashif Jamal Soofi <kashif.soofi+git@gmail.com>
2021-04-21 12:54:53 +0100

    Channel select

---
+++ chapter_09/06_channel_select/channel.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func callerA(c chan string) {
+	c <- "Hello World!"
+	close(c)
+}
+
+func callerB(c chan string) {
+	c <- "Hola Mundo!"
+	close(c)
+}
+
+func main() {
+	a, b := make(chan string), make(chan string)
+	go callerA(a)
+	go callerB(b)
+	var msg string
+	openA, openB := true, true
+	for openA || openB {
+		select {
+		case msg, openA = <-a:
+			if openA {
+				fmt.Printf("%s from A\n", msg)
+			}
+		case msg, openB = <-b:
+			if openB {
+				fmt.Printf("%s from B\n", msg)
+			}
+		}
+	}
+}
